536cdbbdb4f1423165751324759ec5f882ac6f6f
Moreen <moreenkim36@gmail.com>
2020-09-15 12:26:57 +0300

    Implement proptype testing

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -3,7 +3,7 @@ import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
 import Congrats from './Congrats';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
@@ -27,4 +27,9 @@ test('renders non-empty congrats message when `success` prop is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
