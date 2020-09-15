b7974471ddd8ee0019a910b72271429ce72f2800
Moreen <moreenkim36@gmail.com>
2020-09-15 14:51:42 +0300

    Add the guessedwords component to the app

---
+++ src/GuessedWords.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+const GuessedWords = (props) => {
+  return <div />;
+};
+
+GuessedWords.PropTypes = {};
+
+export default GuessedWords;
