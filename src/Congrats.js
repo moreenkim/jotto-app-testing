d9c809894c412b518085752566a57feee9bbd107
Moreen <moreenkim36@gmail.com>
2020-09-15 12:08:25 +0300

    Code and test the component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-export default () => {
-  return <div></div>;
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congrats!! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
