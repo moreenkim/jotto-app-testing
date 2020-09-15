e0bc168e7277d5bc9f60bb70b451a55154c72d93
Moreen <moreenkim36@gmail.com>
2020-09-15 11:50:39 +0300

    Set up the congrats component and test it

---
+++ src/Congrats.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+import Congrats from './Congrats';
+import { findByTestAttr } from '../test/testUtils';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
+test('renders without error', () => {});
+
+test('renders no text when `success` prop is false', () => {});
+
+test('renders non-empty congrats message when `success` prop is true', () => {});
