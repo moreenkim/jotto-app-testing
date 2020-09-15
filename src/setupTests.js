4bc34be197514bc78f5742549f2e258ccfdd73a7
Moreen <moreenkim36@gmail.com>
2020-09-15 14:33:11 +0300

    MOve enzyme config to setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,7 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
