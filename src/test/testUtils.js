187979f76afa7e54c09cf081a835fb905419b724
Moreen <moreenkim36@gmail.com>
2020-09-16 12:35:19 +0300

    Add the store factory and setup start function for input component

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,5 +1,11 @@
 import checkPropTypes from 'check-prop-types';
-//import { checkPropTypes } from 'prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../reducers';
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+};
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
