cf44a4183b7ebbbea5a2ae1a759299a7b130b9ee
Moreen <moreenkim36@gmail.com>
2020-09-17 14:28:16 +0300

    clear the input box on submit

--- src/Input.test.js
+++ src/Input.test.js
@@ -96,4 +96,8 @@ describe('`guessWord` action creator call', () => {
     const guessWordArg = guessWordMock.mock.calls[0][0];
     expect(guessWordArg).toBe(guessedWord);
   });
+
+  test('input box clears on submit', () => {
+    expect(wrapper.instance().inputBox.current.value).toBe('');
+  });
 });
